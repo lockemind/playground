5c72346dff7f4e7f0b06402d5628deeb6acf34c6
lockemind <pedr.amaral@gmail.com>
2015-01-16 18:54:10 +0000

    change route

--- routes/pedro.js
+++ routes/pedro.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var xPosition = 0
+var yPosition = 0
+var color = 0x000000
+
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/cycle', function(req, res) {
 
-  res.send({name:'Pedro', surname:'Amaral'});
+	xPosition++
+	yPosition++  	
+	color++
 
+	if (xPosition == 100) {
+		xPosition = 0
+		yPosition = 0
+		color = 0x000000
+	}
+
+	res.send({x: xPosition, y: yPosition, color: color});
 });
 
 module.exports = router;
