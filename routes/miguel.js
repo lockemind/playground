4e830a19aef710062d713aa73804c656a9a3ffd4
unknown <miguel.leite@alert.pt>
2015-01-16 19:30:00 +0000

    changes

--- routes/miguel.js
+++ routes/miguel.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/miguel', function(req, res) {
-  res.send('Leite', { title: "I'm allways Leite" });
+router.get('/', function(req, res) {
+  res.send("" + (Math.random() * 1000));
 });
 
 module.exports = router;
