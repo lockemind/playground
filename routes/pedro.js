7def08dbd38692e1bbf93875539cdb8967c13076
lockemind <pedr.amaral@gmail.com>
2015-01-17 17:08:03 +0000

    added animated circle

--- routes/pedro.js
+++ routes/pedro.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var xPosition = 0
-var yPosition = 0
-var color = 0x000000
+router.get('/colorCycle', function(req, res) {
 
-/* GET users listing. */
-router.get('/cycle', function(req, res) {
-
-	xPosition++
-	yPosition++  	
-	color++
-
-	if (xPosition == 100) {
-		xPosition = 0
-		yPosition = 0
-		color = 0x000000
-	}
-
-	res.send({x: xPosition, y: yPosition, color: color});
+    var color = "#"+Date.now().toString().slice(7,13)
+	res.send({color: color});
 });
 
 module.exports = router;
