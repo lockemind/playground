5860d90d1073e2807c62d6ff08ca2d696f8a8420
System Administrator <root@mbpderiotavares.mni.local>
2015-01-20 19:05:17 +0000

    ricardo jade update

--- public/javascripts/users/ricardo.js
+++ public/javascripts/users/ricardo.js
@@ -1,8 +1,8 @@
 
 $(function(){
 
-    var $root = $('#tiago')
-    var root = document.getElementById('tiago')
+    var $root = $('#ricardo')
+    var root = document.getElementById('ricardo')
 
     console.log($root.get(0), root)
 })
